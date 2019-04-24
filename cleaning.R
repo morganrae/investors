468612a1bf242c2ccca0a48ba50482714aeb0673
Morgan Edwards <morgan.rae.edwards@gmail.com>
2019-04-24 14:54:25 -0400

    Added .gitignore and skeleton files

---
+++ cleaning.R
@@ -0,0 +1,1 @@
+# Code for cleaning/formatting the data before analysis.
---
+++ descriptive.R
@@ -0,0 +1,8 @@
+# Descriptive statistics comparing types of invesments by fossil fuel and
+# non-fossil fuel companies.
+
+# Compare investment totals
+# Compare investment totals by sector
+# Compare investment totals by tag
+# Compare investment totals by development stage
+# Plot annual investments over time
